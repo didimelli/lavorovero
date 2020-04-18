491fcfef0910f47d2f5162a5fe1045425fe5f65f
Davide <davide.melli@mail.polimi.it>
2020-04-18 16:28:48 +0200

    wip homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="px-64 pt-4 w-full ">
+    <div class="p-2 flex justify-between">
+      <div>
+        <h1 class="font-bold text-3xl">
+          👷🏾‍♂️ lavorovero
+        </h1>
+      </div>
+      <div class="flex justify-between align-bottom">
+        <span>
+          lavorovero
+        </span>
+        <span class="pl-8">
+          chi siamo
+        </span>
+        <span class="pl-8">
+          i nostri prodotti
+        </span>
+        <span class="pl-8">
+          contattaci
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
